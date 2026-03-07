7dd971adcfdb2e5f2d066e848e9dbb013b602950
Muhammad Qais <qaismuhammad742@gmail.com>
2026-03-07 06:12:01 +0500

    add CORS package in it

--- webapi/webapi/App_Start/WebApiConfig.cs
+++ webapi/webapi/App_Start/WebApiConfig.cs
@@ -1,13 +1,15 @@
 ﻿using System.Web.Http;
-
+using System.Web.Http.Cors;
 namespace webapi
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+           
             config.MapHttpAttributeRoutes();
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
+           config.EnableCors(cors);
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
